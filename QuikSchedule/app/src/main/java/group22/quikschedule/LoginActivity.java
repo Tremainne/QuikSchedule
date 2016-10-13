2b8b6bb9cd0c38b4eccb1060d19edb65bfe58f78
David Thomson <thomsondj@hotmail.com>
2016-10-12 17:01:05 -0700

    Created initial login and register activities. Changed .gitignore to allow gradle settings to be shared.

---
+++ QuikSchedule/app/src/main/java/group22/quikschedule/LoginActivity.java
@@ -0,0 +1,13 @@
+package group22.quikschedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); //TODO: Create layout for login page
+    }
+}
---
+++ QuikSchedule/app/src/main/java/group22/quikschedule/RegisterActivity.java
@@ -0,0 +1,15 @@
+package group22.quikschedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register); //TODO: Create layout for register page
+    }
+
+
+}
