c36937d949c8259508f980eeb1390ac458a42291
David Thomson <thomsondj@hotmail.com>
2016-10-16 19:27:10 -0700

    Created parsing algorithm from UCSD MyTritonLink schedule

--- QuikSchedule/app/src/main/java/group22/quikschedule/MainActivity.java
+++ QuikSchedule/app/src/main/java/group22/quikschedule/MainActivity.java
@@ -1,15 +1,9 @@
 package group22.quikschedule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,24 +14,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toWebreg(View view){
-        setContentView(R.layout.activity_webreg);
-
-        Button btn = (Button) findViewById(R.id.submitWebreg);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                retrieve(v);
-            }
-        });
-    }
-
-    public void retrieve(View view){
-        EditText etPID = (EditText) findViewById(R.id.enterPID);
-        String pid = etPID.getText().toString();
-
-        EditText etPass = (EditText) findViewById(R.id.enterPassword);
-        String pass = etPID.getText().toString();
-
-        new WebregScraping().execute(pid, pass);
+        startActivity(new Intent(MainActivity.this, WebregActivity.class));
     }
 }
