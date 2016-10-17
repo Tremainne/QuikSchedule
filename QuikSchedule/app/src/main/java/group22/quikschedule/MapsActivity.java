bf7f006f67623b541927ffaee91da4551d8b6edb
Christoph Steefel <cfsteefel@gmail.com>
2016-10-17 11:49:40 -0700

    Adding Maps framework and activity to repo.

---
+++ QuikSchedule/app/src/main/java/group22/quikschedule/MapsActivity.java
@@ -0,0 +1,41 @@
+package group22.quikschedule;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.app.FragmentTransaction;
+
+
+
+/**
+ * This shows how to create a simple activity with a map and a marker on the map.
+ */
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        MapFragment mapFragment =
+                (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+        MapFragment mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map, mMapFragment);
+        fragmentTransaction.commit();
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
