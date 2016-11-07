439d2b92ceb8569d9e57b3e3f4406a2456375b15
David Thomson <thomsondj@hotmail.com>
2016-11-07 07:54:28 -0800

    Documentation - removed old authentication systems

--- QuikSchedule/app/src/main/java/group22/quikschedule/Settings/SettingsFragment.java
+++ QuikSchedule/app/src/main/java/group22/quikschedule/Settings/SettingsFragment.java
@@ -8,13 +8,34 @@ import android.view.ViewGroup;
 
 import group22.quikschedule.R;
 
-
+/**
+ * Class: SettingsFragment
+ *
+ * Bugs: None known
+ * Version: 1.0
+ * Date: 11/7/16
+ *
+ * Description: Activity that implements various settings buttons such as the connection to WebReg
+ *              and SQLite database. Still requires UI overhaul but functions with back end
+ *              correctly in current state.
+ *
+ * @author David Thomson
+ */
 public class SettingsFragment extends Fragment {
 
+    /**
+     * Description: Default constructor, required as per instructions for Fragment class
+     */
     public SettingsFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Description: Default starting method called when starting a new SettingsFragment
+     * @param inflater - indicates which layer is to be brought to the front
+     * @param container - indicates the current context of the layer
+     * @param savedInstanceState - indicates the context from which the method was called
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
