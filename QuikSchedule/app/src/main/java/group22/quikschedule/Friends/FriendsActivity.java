6f247e52f9c5818c7f439a9e562406ead0c72339
David Thomson <thomsondj@hotmail.com>
2016-11-07 07:54:28 -0800

    Documentation - removed old authentication systems

--- QuikSchedule/app/src/main/java/group22/quikschedule/Friends/FriendsActivity.java
+++ QuikSchedule/app/src/main/java/group22/quikschedule/Friends/FriendsActivity.java
@@ -3,8 +3,17 @@ package group22.quikschedule.Friends;
 import android.support.v4.app.FragmentActivity;
 
 /**
- * Created by David on 10/24/16.
+ * Class: FriendsActivity
+ *
+ * Bugs: None known
+ * Version: 1.0
+ * Date: 11/5/16
+ *
+ * Description: Activity from which the FriendsFragment can extend so that super class methods may
+ *              be overwritten in the fragment.
+ *
+ * @author Tynan Dewes
+ * @author David Thomson
  */
-
 public class FriendsActivity extends FragmentActivity {
 }
--- QuikSchedule/app/src/main/java/group22/quikschedule/MainActivity.java
+++ QuikSchedule/app/src/main/java/group22/quikschedule/MainActivity.java
@@ -1,231 +1,28 @@
 package group22.quikschedule;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
-import group22.quikschedule.Maps.Directions;
-import group22.quikschedule.Settings.WebregActivity;
-
+/**
+ * Class: MainActivity
+ *
+ * Bugs: None known
+ * Version: 1.0
+ * Date: 11/7/16
+ *
+ * Description: Previously was location of authentication systems, since moved to settings and
+ *              friends fragments
+ *
+ * @author David Thomson
+ */
 public class MainActivity extends AppCompatActivity {
 
-    private CallbackManager mCallbackManager;
-    private GoogleApiClient mGoogleApiClient;
-    private GoogleApiClient.OnConnectionFailedListener GoogleConnectionFailedListener;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private String TAG = MainActivity.class.getSimpleName();
-    private int RC_SIGN_IN = 1;
-
+    /**
+     * Description: Default starting method called when starting a new MainActivity
+     * @param savedInstanceState - indicates the context from which the method was called
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        /*super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext()); //Allows for Facebook SDK access
-        FirebaseApp.initializeApp(this);
-        setContentView(R.layout.activity_main);
-        AppEventsLogger.activateApp(this);
-
-        //Initialize Facebook Login + Firebase Connection
-        mCallbackManager = CallbackManager.Factory.create();
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-            }
-        };
-
-        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_login);
-        loginButton.setReadPermissions("email", "public_profile", "user_friends");
-        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                handleFacebookAccessToken(loginResult.getAccessToken());
-            }
-
-            @Override
-            public void onCancel() {
-                Log.d(TAG, "facebook:onCancel");
-                // ...
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Log.d(TAG, "facebook:onError", error);
-                // ...
-            }
-        });
-        //End Facebook Login + Firebase Connection
-
-        //Initialize Google Login
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(MainActivity.this, GoogleConnectionFailedListener)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        GoogleConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
-            @Override
-            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                //TODO
-                Log.d(TAG, "google:onConnectionFailed " + connectionResult.toString());
-            }
-        };
-
-        SignInButton signInButton = (SignInButton) findViewById(R.id.google_login);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
-        signInButton.setScopes(gso.getScopeArray());
-        findViewById(R.id.google_login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.google_login:
-                        signIn();
-                        break;
-                }
-            }
-        });
-
-        //End Google Login
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-
-    public void toWebreg(View view){
-        startActivity(new Intent(MainActivity.this, WebregActivity.class));
-    }
-
-    private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
-
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            firebaseAuthWithGoogle(acct);
-
-            //Status of log in, add temporary button
-            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-
-            //Create manual refresh method with boolean parameter
-            //updateUI(true);
-        } else {
-            // Signed out, show unauthenticated UI.
-            //updateUI(false);
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });*/
     }
 }
