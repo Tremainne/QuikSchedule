130b166aaa14873ab92e00f88f7411fcc45de606
Ishjot Suri <ishjotsuri@Ishjot-Suris-Macbook-Air-3.local>
2016-11-14 11:45:36 -0800

    Potty

--- QuikSchedule/app/src/main/java/group22/quikschedule/Calendar/DatabaseHelper.java
+++ QuikSchedule/app/src/main/java/group22/quikschedule/Calendar/DatabaseHelper.java
@@ -1,8 +1,17 @@
 package group22.quikschedule.Calendar;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.google.api.client.json.Json;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by kris on 10/22/16.
@@ -25,6 +34,79 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 DatabaseContract.DatabaseEntry.COLUMN_DATA + " TEXT NOT NULL);"
         );
     }
+
+    public JSONArray cursorToJson(Cursor cursor)
+    {
+        JSONArray resultSet     = new JSONArray();
+
+        cursor.moveToFirst();
+        while (cursor.isAfterLast() == false) {
+
+            int totalColumn = cursor.getColumnCount();
+            JSONObject rowObject = new JSONObject();
+
+            for( int i=0 ;  i< totalColumn ; i++ )
+            {
+                if( cursor.getColumnName(i) != null )
+                {
+                    try
+                    {
+                        if( cursor.getString(i) != null )
+                        {
+                            Log.d("TAG_NAME", cursor.getString(i) );
+                            rowObject.put(cursor.getColumnName(i) ,  cursor.getString(i) );
+                        }
+                        else
+                        {
+                            rowObject.put( cursor.getColumnName(i) ,  "" );
+                        }
+                    }
+                    catch( Exception e )
+                    {
+                        Log.d("TAG_NAME", e.getMessage()  );
+                    }
+                }
+            }
+            resultSet.put(rowObject);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return resultSet;
+    }
+
+    public ArrayList<JSONArray> getData(Context mContext)
+    {
+        DatabaseHelper mDbHelper = new DatabaseHelper(mContext);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        String response;
+
+        String sql = "SELECT " + DatabaseContract.DatabaseEntry.COLUMN_DATA + " FROM " +
+                DatabaseContract.DatabaseEntry.TABLE_NAME + "WHERE"; // + " WHERE " +
+        //DatabaseContract.DatabaseEntry.COLUMN_DAY; // + " IS MONDAY";
+
+
+        ArrayList<JSONArray> object_day = new ArrayList<JSONArray>();
+
+        Cursor cursor = db.rawQuery(sql, null); //+ "MONDAY", null);
+        object_day.add(cursorToJson(cursor));
+
+        /*cursor = db.rawQuery(sql + "TUESDAY", null);
+        object_day.add(cursorToJson(cursor));
+
+        cursor = db.rawQuery(sql + "WEDNESDAY", null);
+        object_day.add(cursorToJson(cursor));
+
+        cursor = db.rawQuery(sql + "THURSDAY", null);
+        object_day.add(cursorToJson(cursor));
+
+        cursor = db.rawQuery(sql + "FRIDAY", null);
+        object_day.add(cursorToJson(cursor));*/
+
+        System.err.println(object_day.get(0));
+        return object_day;
+
+    }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
